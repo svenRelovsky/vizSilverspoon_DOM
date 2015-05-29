ecb3bb55f6150985f01b3c35a6264854f2061a1f
Burda <m.burda583@gmail.com>
2015-05-29 21:39:28 +0200

    oprava dalsej sipky

--- SilverSpoon Visualiser/src/PathCreation/PathCreator.java
+++ SilverSpoon Visualiser/src/PathCreation/PathCreator.java
@@ -491,9 +491,9 @@ public class PathCreator {
         switch (model) {
             case Constants.RASPBERRY_PI_BOARD:
                 g0 = doc.createElement("line");
-                g0.setAttribute("x1", String.valueOf(currPos.x+15));
+                g0.setAttribute("x1", String.valueOf(currPos.x+1));
                 g0.setAttribute("y1", String.valueOf(currPos.y));
-                g0.setAttribute("x2", String.valueOf(currPos.x+15));
+                g0.setAttribute("x2", String.valueOf(currPos.x+1));
                 g0.setAttribute("y2", "80");
                 g0.setAttribute("style", "stroke:rgb(0,0,255);stroke-width:1");
                 group.appendChild(g0);
@@ -755,9 +755,9 @@ public class PathCreator {
                         yCoord = 5200;
                     }
                     g = doc.createElement("line");
-                    g.setAttribute("x1", String.valueOf(currPos.x + 35));
+                    g.setAttribute("x1", String.valueOf(currPos.x + 25));
                     g.setAttribute("y1", String.valueOf(currPos.y));
-                    g.setAttribute("x2", String.valueOf(currPos.x + 35));
+                    g.setAttribute("x2", String.valueOf(currPos.x + 25));
                     g.setAttribute("y2", "5100");
                     g.setAttribute("style", "stroke:rgb(0,0,255);stroke-width:25");
                     group.appendChild(g);
